54a6b050b93cb8c61b81937888b5553b43ceb759
Ansh Shrestha <anshshrestha15@gmail.com>
2026-06-21 18:47:15 +0545

    chore: implemented lint-staged in husky

--- Task5/src/main.ts
+++ Task5/src/main.ts
@@ -2,11 +2,11 @@ import { type Expense } from './type/type';
 import { getExpenses, addExpense } from './data/data';
 
 const Expense1: Expense = {
-  id: '3',
-  title: 'BhatBhatini groceries',
+  id: '4',
+  title: 'Electricity Bill',
   amount: 5000,
-  category: 'Groceries',
+  category: 'Bills',
   date: `${new Date().getDate()}`,
 };
-// addExpense(Expense1);
+addExpense(Expense1);
 console.log(getExpenses());
