654ce55e65635b21573de649d1cd25137912159f
Ansh Shrestha <anshshrestha15@gmail.com>
2026-07-03 14:43:10 +0545

    feat(task9): Create navigationBar with logo implemented

--- task9/src/components/NavBar.tsx
+++ task9/src/components/NavBar.tsx
@@ -1,21 +1,42 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
+import { Link } from "@tanstack/react-router";
+import { Button } from "./ui/button";
 
 export const NavBar = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem className="flex">
-          <NavigationMenuLink>Home</NavigationMenuLink>
-          <NavigationMenuLink>Products</NavigationMenuLink>
-          <NavigationMenuLink>Cart</NavigationMenuLink>
-          <NavigationMenuLink>Login/Register</NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div>
+      <nav className="items-center justify-center flex bg-sidebar-primary-foreground text-white space-x-5 py-5">
+        <img
+          src="public/logo.svg"
+          className="w-30 h-30 flex absolute left-3"
+        ></img>
+        <Link
+          to="/"
+          activeOptions={{ exact: true }}
+          className="[&.active]:[&>button]:bg-primary [&.active]:[&>button]:text-primary-foreground"
+        >
+          <Button variant="link" className="bg-sidebar-primary-foreground">
+            Home
+          </Button>
+        </Link>
+        <Link
+          to="/products"
+          activeOptions={{ exact: true }}
+          className="[&.active]:[&>button]:bg-primary [&.active]:[&>button]:text-primary-foreground"
+        >
+          <Button variant="link" className="bg-sidebar-primary-foreground">
+            Products
+          </Button>
+        </Link>
+        <Link
+          to="/login"
+          activeOptions={{ exact: true }}
+          className="[&.active]:[&>button]:bg-primary [&.active]:[&>button]:text-primary-foreground"
+        >
+          <Button variant="link" className="bg-sidebar-primary-foreground">
+            Login
+          </Button>
+        </Link>
+      </nav>
+    </div>
   );
 };
