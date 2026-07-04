ca933a64db9857867c15da78f6180202e2abfcc0
Ansh Shrestha <anshshrestha15@gmail.com>
2026-07-04 14:42:39 +0545

    feat(task9): Home page updated

--- task9/src/pages/Home.tsx
+++ task9/src/pages/Home.tsx
@@ -1,17 +1,28 @@
 export const Home = () => {
   return (
-    <div className="flex flex-col">
-      <div>
-        <div className="flex flex-col h-1/3">
-          <h1>Welcome to Home Page</h1>
-          <p className="relative h-full w-1/2">
-            This is a E-Commerce website built using FakeStoreAPI. Here u can
-            experience various features like infiniteScroll, ReactContextAPI,
-            Tanstack Router,Tanstack Query, React Hook Forms and Zod, etc.
+    <div className="min-h-120 flex items-center justify-center px-10 mt-3">
+      <div className="flex flex-col md:flex-row items-center gap-12 max-w-7xl">
+        <div className="flex-1 space-y-6">
+          <h1 className="text-4xl font-bold">
+            Welcome to Our <br /> E-Commerce Store
+          </h1>
+
+          <p className="text-lg text-muted-foreground">
+            This is an E-Commerce website built using FakeStoreAPI. Here you can
+            experience features such as Infinite Scroll, React Context API,
+            TanStack Router, TanStack Query, React Hook Form, Zod validation,
+            and much more.
           </p>
         </div>
+
+        <div className="flex-1 flex justify-center">
+          <img
+            src="/ecom.jpg"
+            alt="E-Commerce"
+            className="w-full max-w-lg rounded-xl shadow-lg"
+          />
+        </div>
       </div>
-      <img src="public/ecom.jpg w-1/2 h-full"></img>
     </div>
   );
 };
