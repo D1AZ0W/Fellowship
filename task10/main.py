2d59cffefce0ea6c60284608d46a041c22c34d7b
Ansh Shrestha <anshshrestha15@gmail.com>
2026-07-09 18:38:12 +0545

    feat(task10): added weights logic

--- task10/main.py
+++ task10/main.py
@@ -5,16 +5,6 @@ from src import inventory_management as i
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
 
-# --- CONFIGURATION ---
-WEIGHTS = {
-    "gold coin": 0.01,
-    "rope": 2.0,
-    "dagger": 3.0,
-    "ruby": 0.5,
-    "iron ore": 10.0,
-}
-MAX_WEIGHT = 50.0
-
 
 def main():
     # Load Inventory
@@ -37,21 +27,20 @@ def main():
         "iron ore",
     ]
 
-    my_inventory = i.add_to_inventory(my_inventory, incomming_loot)
+    my_inventory, items_dropped = i.add_to_inventory(
+        my_inventory, incomming_loot
+    )
 
-    print(f"Loaded Inventory : {my_inventory}")
-    i.generate_report(my_inventory)
+    if items_dropped:
+        logger.warning(f"Max weight reached! Left behind: {items_dropped}")
 
-    # if items_dropped:
-    #     logger.warning(f"Max weight reached! Left behind: {items_dropped}")
+    # Display Inventory
+    print("\n--- Visual Report ---")
+    i.display_inventory(my_inventory)
 
-    # # Display Inventory
-    # print("\n--- Visual Report ---")
-    # display_inventory(my_inventory)
-
-    # # Save Inventory to a file
-    # print("---Exporting Data ---")
-    # save_inventory(my_inventory, "inventory.json")
+    # Save Inventory to a file
+    print("---Exporting Data ---")
+    i.save_inventory(my_inventory, "inventory.json")
 
 
 if __name__ == "__main__":
--- task10/src/inventory_management.py
+++ task10/src/inventory_management.py
@@ -4,6 +4,17 @@ import json
 path_inventory = "src/inventory.json"
 
 
+# --- CONFIGURATION ---
+WEIGHTS = {
+    "gold coin": 0.01,
+    "rope": 2.0,
+    "dagger": 3.0,
+    "ruby": 0.5,
+    "iron ore": 10.0,
+}
+MAX_WEIGHT = 50.0
+
+
 def load_inventory(fileName):
     try:
         with open(path_inventory) as f:
@@ -21,18 +32,22 @@ def add_to_inventory(inventory, new_items):
     try:
         if not (isinstance(new_items, list) or isinstance(new_items, list)):
             raise ValueError
+        items_dropped = []
         for item in new_items:
             if item == "" or isinstance(item, int or float) or item is None:
                 continue
+            if calc_current_weight(inventory) >= 50.0:
+                items_dropped.append(item)
+
             current = item.lower().strip()
             current_quantity = inventory.get(current)
             if current_quantity is not None:
                 quantity = current_quantity + 1
             else:
                 quantity = 1
+            print(f"added {current} to inventory. Total in bag {quantity}")
             inventory[current] = quantity
-
-        return inventory
+        return inventory, items_dropped
     except ValueError:
         print("Error: loot items should be in list or dictionary")
         return inventory
@@ -50,7 +65,7 @@ def border(func):
 
 
 @border
-def generate_report(inventory):
+def display_inventory(inventory):
     c = 0
     for item in inventory:
         print(f"| {item:<10} x{inventory[item]:>3} |", end="")
@@ -63,5 +78,24 @@ def save_inventory(inventory, fileName):
     try:
         with open(path_inventory, "w") as f:
             f.write(json.dumps(inventory))
+        print("Saved to inventory")
     except FileNotFoundError:
-        print("error")
+        print("Failed to save to inventory")
+
+
+def weight_calc(item):
+    try:
+        if not isinstance(item, str):
+            raise ValueError
+        return float(WEIGHTS.get(item, 1))
+
+    except ValueError:
+        print("Item should be string")
+        return 1
+
+
+def calc_current_weight(inventory):
+    total_weight = 0.0
+    for item in inventory:
+        total_weight += weight_calc(item)
+    return total_weight
