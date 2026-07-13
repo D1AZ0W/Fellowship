80f0d1e621b04788922f45182058c8beceee0558
Ansh Shrestha <anshshrestha15@gmail.com>
2026-07-13 10:27:45 +0545

    feat(task11): add trigger as well as table for PriceHistory

--- task11/trigger.sql
+++ task11/trigger.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION check_quantity()
+CREATE OR REPLACE FUNCTION check_quantity()
 RETURNS TRIGGER AS $$
 BEGIN
     IF NEW.Quantity <= 0 THEN
@@ -13,7 +13,7 @@ BEFORE INSERT OR UPDATE ON OrderDetails
 FOR EACH ROW
 EXECUTE FUNCTION check_quantity();
 
-CREATE FUNCTION validate_date()
+CREATE OR REPLACE FUNCTION validate_date()
 RETURNS TRIGGER AS $$
 BEGIN
     IF NEW.OrderDate > CURRENT_DATE THEN
@@ -28,3 +28,28 @@ BEFORE INSERT OR UPDATE
 ON Orders
 FOR EACH ROW
 EXECUTE FUNCTION validate_date();
+
+CREATE TABLE IF NOT EXISTS PriceHistory(
+    HistoryID SERIAL PRIMARY KEY, 
+    ProductID integer NOT NULL REFERENCES Products(ProductID),
+    OldPrice NUMERIC(10,2),
+    NewPrice NUMERIC(10,2),
+    ChangeOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION track_price()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.Price != OLD.price THEN
+        INSERT INTO PriceHistory (ProductID,OldPrice,NewPrice) VALUES (OLD.ProductID,OLD.Price,NEW.Price);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+    
+
+CREATE TRIGGER PriceHistory
+AFTER UPDATE
+ON Products
+FOR EACH ROW
+EXECUTE FUNCTION track_price();
