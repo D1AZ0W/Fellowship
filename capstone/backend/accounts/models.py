288c1b7c2938497aca7390d0ec2b9ba49fd37459
Ansh Shrestha <anshshrestha15@gmail.com>
2026-07-22 12:16:56 +0545

    feat: add model for users

---
+++ capstone/backend/accounts/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class Users(AbstractUser):
+	profile_picture = models.ImageField(
+		upload_to='profiles/',
+		blank=True,
+		null=True,
+	)
+
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.username
