7168acc23d231ef687660858fdcdaae9e09fab4e
Ansh Shrestha <anshshrestha15@gmail.com>
2026-07-23 23:21:36 +0545

    feat(capstone): add protected routes and theme improve

--- capstone/frontend/src/routes/_app.tsx
+++ capstone/frontend/src/routes/_app.tsx
@@ -1,7 +1,15 @@
-import { Outlet, createFileRoute } from '@tanstack/react-router'
+import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
 import { AppLayout } from '#/components/layout/AppLayout'
 
 export const Route = createFileRoute('/_app')({
+  beforeLoad: () => {
+    const token = localStorage.getItem('access_token')
+    if (!token) {
+      throw redirect({
+        to: '/login',
+      })
+    }
+  },
   component: AppRoute,
 })
 
--- capstone/frontend/src/routes/_guest.tsx
+++ capstone/frontend/src/routes/_guest.tsx
@@ -1,10 +1,15 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 import { GuestLayout } from '#/components/layout/GuestLayout'
 
 export const Route = createFileRoute('/_guest')({
+  beforeLoad: () => {
+    const token = localStorage.getItem('access_token')
+
+    if (token) {
+      throw redirect({
+        to: '/dashboard',
+      })
+    }
+  },
   component: GuestLayout,
 })
-
-export function Guest() {
-  return <Outlet />
-}
