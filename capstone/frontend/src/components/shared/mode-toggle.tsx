42fbf1a65ce270adc81c7ccd3d3e17667db0373d
Ansh Shrestha <anshshrestha15@gmail.com>
2026-08-02 22:01:44 +0545

    fix(capstone): fix hydration error theme

--- capstone/frontend/src/components/shared/mode-toggle.tsx
+++ capstone/frontend/src/components/shared/mode-toggle.tsx
@@ -10,18 +10,19 @@ import {
 import { useTheme } from '@/components/theme-provider'
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <Button variant="outline" size="icon">
-          <>
-            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          </>
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+        {theme === 'dark' && (
+          <Sun className="h-[1.2rem] w-[1.2rem]   transition-all " />
+        )}
+        {theme === 'light' && (
+          <Moon className=" h-[1.2rem] w-[1.2rem] transition-all " />
+        )}
+
+        <span className="sr-only">Toggle theme</span>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme('light')}>
